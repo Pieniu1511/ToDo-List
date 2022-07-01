cd1fa600797d073e4ff42efb80ba3f4d59cacb5f
Edmund <pieniu1511@gmail.com>
2022-07-01 13:11:39 +0200

    todo list completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let todoInput, errorInfo, addBtn, ulList
+let todoInput, errorInfo, addBtn, ulList, popup, popupInfo, todoToEdit, popupInput, popupAddBtn, popupCloseBtn
 
 const main = () => {
 	prepareDOMElements()
@@ -10,10 +10,19 @@ const prepareDOMElements = () => {
 	errorInfo = document.querySelector('.error-info')
 	addBtn = document.querySelector('.btn-add')
 	ulList = document.querySelector('.todolist ul')
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
 }
 
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTado)	
+	document.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
 }
 
 const addNewTado = () => {
@@ -47,6 +56,53 @@ const createToolArea = (newTodo) => {
 	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = (e) => {
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
+	popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = ''
+}
+
+const changeTodoText = (params) => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	}	else {
+		popupInfo.textContent = 'Musisz podać jakąś treść'
+	}
+}
+
+const deleteTodo = (e) => {
+	e.target.closest('li').remove()
+
+	const allTodos = ulList.querySelectorAll('li')
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście.'
+	}
+}
+
+const enterKeyCheck = e => {
+	if (e.key === 'Enter') {
+		addNewTado()
+	}
 }
 
 
